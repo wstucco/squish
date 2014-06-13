ae3fb05785762ee731a5b87a6b38e61acec4501d
Massimo Ronca <massimo@mikamai.com>
2014-06-13 17:42:11 +0200

    Added Router.ServeHTTP test

--- router_test.go
+++ router_test.go
@@ -1,6 +1,10 @@
 package squish
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
 	assert "github.com/pilu/miniassert"
 
 	"testing"
@@ -12,16 +16,41 @@ func TestNew(t *testing.T) {
 	assert.Type(t, "*squish.Router", r)
 	assert.Type(t, "[]*squish.Route", r.routes)
 
-	assert.Equal(t, 0, len(router.routes))
+	assert.Equal(t, 0, len(r.routes))
 
 	emptyArray := make([]*Route, 0)
-	assert.Equal(t, emptyArray, router.routes)
+	assert.Equal(t, emptyArray, r.routes)
 }
 
 func TestRouter_AddRoute(t *testing.T) {
 	r := New()
 
 	r.AddRoute(F)
-	assert.Equal(t, 1, len(router.routes))
-	assert.Type(t, "*squish.Route", router.routes[0])
+	assert.Equal(t, 1, len(r.routes))
+	assert.Type(t, "*squish.Route", r.routes[0])
+}
+
+func TestRouter_ServeHTTP(t *testing.T) {
+	r := New()
+	request, _ := http.NewRequest("GET", "/", nil)
+	recorder := httptest.NewRecorder()
+	r.ServeHTTP(recorder, request)
+
+	assert.Equal(t, "", string(recorder.Body.Bytes()))
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	matcher := func(req *Request) bool {
+		return req.URL.Path == "/test"
+	}
+
+	handler := func(req *Request, res Response) {
+		fmt.Fprint(res, "test body")
+	}
+
+	r.AddRoute(matcher, handler)
+
+	request, _ = http.NewRequest("GET", "/test", nil)
+	recorder = httptest.NewRecorder()
+	r.ServeHTTP(recorder, request)
+	assert.Equal(t, "test body", string(recorder.Body.Bytes()))
 }
