dfc37767254ce693b07e23572ab0c91f72016632
Massimo Ronca <massimo@mikamai.com>
2014-06-14 14:07:38 +0200

    Added Match function to Request

--- request.go
+++ request.go
@@ -2,6 +2,8 @@ package squish
 
 import (
 	"net/http"
+	"net/url"
+	"regexp"
 )
 
 type Request struct {
@@ -12,4 +14,30 @@ func NewRequest(r *http.Request) *Request {
 	return &Request{
 		Request: r,
 	}
+}
+
+func (r *Request) Path() string {
+	return r.Request.URL.Path
+}
+
+func (r *Request) Match(pattern string) (bool, url.Values) {
+	values := make(url.Values)
+
+	matcher := regexp.MustCompile(pathToRegexpString(pattern))
+	matches := matcher.FindAllStringSubmatch(r.Path(), -1)
+	if matches != nil {
+		names := matcher.SubexpNames()
+		for i := 1; i < len(names); i++ {
+			name := names[i]
+			value := matches[0][i]
+			if len(name) > 0 && len(value) > 0 {
+				values.Set(name, value)
+			}
+		}
+
+		return true, values
+	}
+
+	return false, values
+
 }
--- request_test.go
+++ request_test.go
@@ -1,6 +1,7 @@
 package squish
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -12,4 +13,61 @@ func TestRequest(t *testing.T) {
 	r := NewRequest(httpRequest)
 
 	assert.Equal(t, r.Request, httpRequest)
+}
+
+func TestRequest_Path(t *testing.T) {
+	httpRequest, _ := http.NewRequest("GET", "http://example.com/test/path", nil)
+	r := NewRequest(httpRequest)
+
+	assert.Equal(t, r.Path(), "/test/path")
+}
+
+func TestRequest_Match(t *testing.T) {
+	tests := [][]string{
+		{
+			"/",
+			"/",
+			"",
+		},
+		{
+			"/:foo/?",
+			"/bar",
+			"foo=bar",
+		},
+		{
+			"/:foo/?",
+			"/bar/", // with trailing slash
+			"foo=bar",
+		},
+		{
+			"/categories/:category_id/posts/:id",
+			"/categories/foo/posts/bar",
+			"category_id=foo&id=bar",
+		},
+		{
+			"/pages/:page_path*",
+			"/pages/foo/bar/baz",
+			"page_path=foo%2Fbar%2Fbaz",
+		},
+		{
+			"/pages/:page.html",
+			"/pages/foo.html",
+			"page=foo",
+		},
+	}
+
+	for _, opts := range tests {
+		routePath := opts[0]
+		requestPath := opts[1]
+		expectedQuery := opts[2]
+
+		httpRequest, _ := http.NewRequest("GET", fmt.Sprintf("http://example.com%s", requestPath), nil)
+		r := NewRequest(httpRequest)
+
+		assert.Equal(t, r.Path(), requestPath)
+
+		match, values := r.Match(routePath)
+		assert.True(t, match)
+		assert.Equal(t, values.Encode(), expectedQuery)
+	}
 }
